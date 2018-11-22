69bc813a5a72ab241baf649d9e914bc0e62dbbfe
Jorge <jotagmzn@gmail.com>
2018-11-22 04:03:46 +0100

    Updates

---
+++ precache-manifest.4952663d248a0a12c912c12c56dbce90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f12247193cf54c6bb56e3836170ced85",
+    "url": "/PixelDrawer/static/media/logo.f1224719.png"
+  },
+  {
+    "revision": "2f956313acabc6b21fab",
+    "url": "/PixelDrawer/static/js/runtime~main.2f956313.js"
+  },
+  {
+    "revision": "15c51db5cef14369ef33",
+    "url": "/PixelDrawer/static/js/main.15c51db5.chunk.js"
+  },
+  {
+    "revision": "9863fb7c8ed567f465dd",
+    "url": "/PixelDrawer/static/js/1.9863fb7c.chunk.js"
+  },
+  {
+    "revision": "15c51db5cef14369ef33",
+    "url": "/PixelDrawer/static/css/main.5fc4383d.chunk.css"
+  },
+  {
+    "revision": "7e029e91bc4d07462279e1d559d27b0e",
+    "url": "/PixelDrawer/index.html"
+  }
+];
